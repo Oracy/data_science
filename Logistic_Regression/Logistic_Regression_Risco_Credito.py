c9040dcf00040f732a6d16c05e3b2954c1d05266
Oracy Martos <oramartos_21@hotmail.com>
2019-12-02 17:46:44 -0300

    Updates

---
+++ Logistic_Regression/Logistic_Regression_Risco_Credito.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 10:36:08 2019
+
+@author: martoso
+"""
+
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv('r')
